b92d9377c6839219a1dc90f82acc2d7061b1d0bf
ablakulova <mablaqulova@gmail.com>
2022-02-14 03:52:49 +0500

    update method fixed

--- CsvApplication/CsvApplication.BLL/Services/EmployeeService.cs
+++ CsvApplication/CsvApplication.BLL/Services/EmployeeService.cs
@@ -1,78 +1,51 @@
 ﻿using CsvApplication.BLL.Interfaces;
-using CsvApplication.DAL.DbContexts;
 using CsvApplication.DAL.Entities;
-using Microsoft.EntityFrameworkCore;
+using CsvApplication.DAL.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CsvApplication.BLL.Services
 {
     public class EmployeeService : IEmployeeService
     {
-        private readonly EmployeeDbContext _dbContext;
+        private readonly IEmployeeRepository _repository;
 
-        public EmployeeService(EmployeeDbContext dbContext)
+        public EmployeeService(IEmployeeRepository repository)
         {
-            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
 
         public async Task<IEnumerable<Employee>> GetAllEmployees()
         {
-            var employees = await _dbContext.Employees.ToListAsync();
-            return employees;
+            return await _repository.GetAll();
         }
 
         public async Task<Employee> GetEmployeeById(string Id)
         {
-            var employee = await _dbContext.Employees.FirstOrDefaultAsync(a => a.Id == Id);
-            return employee;
+            return await _repository.GetById(int.Parse(Id));
         }
 
         public async Task<Employee> UpdateEmployee(Employee employee)
         {
-            var result = await _dbContext.Employees
-                .FirstOrDefaultAsync(e => e.Id == employee.Id);
+            var result = await _repository.Update(employee);
+            await _repository.SaveAsync();
 
-            if (result != null)
-            {
-                result.PayrollNumber = employee.PayrollNumber;
-                result.FirstName = employee.FirstName;
-                result.SurName = employee.SurName;
-                result.DateOfBirth = employee.DateOfBirth;
-                result.Telephone = employee.Telephone;
-                result.Mobile = employee.Mobile;
-                result.Address = employee.Address;
-                result.Address2 = employee.Address2;
-                result.PostCode = employee.PostCode;
-                result.EmailHome = employee.EmailHome;
-                result.StartDate = employee.StartDate;
-
-                await _dbContext.SaveChangesAsync();
-
-                return result;
-            }
-
-            return null;
+            return result;
         }
 
         public async Task<Employee> AddEmployee(Employee employee)
         {
-            var emp = await _dbContext.AddAsync(employee);
-            await _dbContext.SaveChangesAsync();
+            var emp = await _repository.Add(employee);
+            await _repository.SaveAsync();
 
-            return emp.Entity;
+            return emp;
         }
 
-        public async Task DeleteEmployee(string Id)
+        public async Task DeleteEmployee(string id)
         {
-            var emp = await _dbContext.Employees.FirstOrDefaultAsync(a => a.Id == Id);
-            if (emp != null)
-            {
-                _dbContext.Employees.Remove(emp);
-                await _dbContext.SaveChangesAsync();
-            }
+            await _repository.Delete(int.Parse(id));
+            await _repository.SaveAsync();
         }
     }
 }
